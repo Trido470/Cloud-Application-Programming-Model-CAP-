6d402712f1dee29d6ab0f7dd7ab8589e03a7b843
Игорь Холодов <trido470@gmail.com>
2026-02-10 14:30:30 +0000

    Add SAP Fiori Elements UIs

---
+++ app/project1/webapp/test/integration/pages/JourneyRunner.js
@@ -0,0 +1,19 @@
+sap.ui.define([
+    "sap/fe/test/JourneyRunner",
+	"ns/project1/test/integration/pages/IncidentsList",
+	"ns/project1/test/integration/pages/IncidentsObjectPage"
+], function (JourneyRunner, IncidentsList, IncidentsObjectPage) {
+    'use strict';
+
+    var runner = new JourneyRunner({
+        launchUrl: sap.ui.require.toUrl('ns/project1') + '/test/flp.html#app-preview',
+        pages: {
+			onTheIncidentsList: IncidentsList,
+			onTheIncidentsObjectPage: IncidentsObjectPage
+        },
+        async: true
+    });
+
+    return runner;
+});
+
